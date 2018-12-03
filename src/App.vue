c31259109f8287a8ca53a972e5b58edeb5322ff5
s17001 <s17001@std.it-college.ac.jp>
2018-12-03 13:05:16 +0900

    farst commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Editer></Editer>
+  </div>
+</template>
+
+<script>
+import Editer from './components/Editor'
+
+export default {
+  name: 'App',
+  components: {
+    Editer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,79 @@
+<template>
+<div id="app">
+  <div class="editor">
+    <mavon-editor ></mavon-editor>
+  </div>
+  <button @click="getMsg">getMsg</button>
+  <div>{{ message }}</div>
+</div>
+  </template>
+
+<script>
+  import Vue from 'vue'
+  import mavonEditor from 'mavon-editor'
+  import 'mavon-editor/dist/css/index.css'
+
+  Vue.use(mavonEditor)
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+        //mavon-editor
+        toolbars: {
+          bold: false,
+        },
+        //other
+        message: ''
+      }
+    },
+    methods: {
+      $imgAdd(pos, $file) {
+        alert('upload image');
+      },
+      $change(value, reader) {
+        this.message = value
+      },
+      getMsg() {
+        console.log(this.message)
+      }
+    },
+  })
+
+  // export default {
+  //   name: 'suggest',
+  //   data () {
+  //     return {
+  //     }
+  //   },
+  //   methods : {
+  //
+  //   },
+  //   comments : {
+  //     mavonEditor
+  //   }
+  // }
+
+
+  /*
+  *
+  *   export default {
+    name: 'suggest',
+    data () {
+      return {
+        emojiList: emojiOrigin,
+        preConvertEmoji: ""
+      }
+    },
+    methods: {
+
+      enter: function(data){
+
+        this.$emit('enter', data.target.value)
+
+      }
+    }
+  }
+  *
+  * */
+
+</script>
